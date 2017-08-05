f0400440654e9aa382a3d2a558cb2fb2d755883f
Sushant101 <rathisushant5@gmail.com>
2017-08-05 21:08:36 +0530

    Added function description in features.py and data_parser.py

--- data_parser.py
+++ data_parser.py
@@ -1,1 +1,10 @@
 import os
+import features
+def load(fileName):
+    #Return a list of lists, each list corresponding to a sentence in the fileName
+    #The list has the format (word,category,label) for each word in sentence, each label corresponding to
+    #the entity of the word. For current training datasets, these are-
+    #(Date-Date, Num-Number of tickets, Dest-Destination, Src-Source Location)
+    #category is obtained from features.category() function
+    pass
+
--- features.py
+++ features.py
@@ -1,1 +1,12 @@
 import os
+def isdigit(word):
+    pass
+
+def frequencies(fileName):
+    #return a dictionary of word frequencies in the file
+    pass
+
+def category(word):
+    #category is retreived from the Hindi WordNet database (text file),return N for noun,
+    #V for verb,AV for adverb and AJ for adjective. Return X if not found in the database
+    pass
