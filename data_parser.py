9ccdde470b24eb7ada9ff9c7ada68741e1e4edbf
ankurshaswat <ankurshaswat@yahoo.co.in>
2017-08-05 21:48:40 +0530

    completed helper functions and also added pickles

--- data_parser.py
+++ data_parser.py
@@ -1,10 +1,36 @@
 import os
 import features
+import string
+
+
 def load(fileName):
     #Return a list of lists, each list corresponding to a sentence in the fileName
     #The list has the format (word,category,label) for each word in sentence, each label corresponding to
     #the entity of the word. For current training datasets, these are-
     #(Date-Date, Num-Number of tickets, Dest-Destination, Src-Source Location)
     #category is obtained from features.category() function
-    pass
+    list=[]
+    
+    ##### change this code so that pickle file is loaded minimum number of times
+    pickle_file = 'list_of_words.pickle'
+    with open(pickle_file, 'rb') as f:
+      save = pickle.load(f)
+      noun_list=save['noun']
+      adverb_list=save['adverb']
+      verb_list=save['verb']
+      adjective_list=save['adjective']
+      del save  # hint to help gc free up memory
+    with open(fileName,'r') as file:
+        x=file.read()
+        x=x.splitlines()
+        for line in x:
+            line_list=[]
+            line=line.translate(None, string.punctuation)
+            words=x.split(' ')
+            for word in words:
+                cat=category(word)
+                
+                line_list.append([word,cat,label])
+            list.append(line_list)
+    return line_list
 
--- features.py
+++ features.py
@@ -1,12 +1,46 @@
 import os
-def isdigit(word):
-    pass
+import string
+
+#def isdigit(word):
+ #   pass
 
 def frequencies(fileName):
     #return a dictionary of word frequencies in the file
-    pass
+    dict={}
+    with open(filename,'r') as file:
+    #     while True:
+        y=file.read()
+        y=y.splitlines()
+    #         x.rstrip()
+        for x in y:
+            x=x.translate(None, string.punctuation)
+    #         x=x.split(' ')
+    #         if x=='':
+    #             break
+            x=x.split(' ')
+    #         print x
+    #         y=[]
+    #         for i in x:
+    #           y.append(i.encode('utf-8'))
+    #         print x[0]
+            for i in x:
+                if i in dict:
+                    dict[i]+=1
+                else:
+                    dict[i]=1
+    return dict
 
 def category(word):
+    if word in noun_list:
+        return 'N'
+    if word in verb_list:
+        return 'V'
+    if word in adverb_list:
+        return 'AV'
+    if word in adjective_list:
+        return 'AJ'
+    return 'X'
     #category is retreived from the Hindi WordNet database (text file),return N for noun,
     #V for verb,AV for adverb and AJ for adjective. Return X if not found in the database
+    
     pass
